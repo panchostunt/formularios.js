83650059990de36b5ac8bdbffcfc4dea93c48f9a
panchostunt <javierardila1@colegioguanenta.edu.co>
2022-05-09 16:15:01 -0500

    ciclo while

---
+++ js/codigo4_js.js
@@ -0,0 +1,16 @@
+let impar = "";
+let i = 1;
+
+function ciclowhile()
+{
+    numero = parseInt(document.formulario.caja.value);
+    while(i<=numero)
+    {
+        if(i%2!==0)
+        {
+            impar = impar + i + ", ";
+        }
+        i = i+1;
+    }
+    document.formulario.respuesta.value = impar;
+}
